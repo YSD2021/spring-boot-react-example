9c54208437ccae196ad137e8de1118b55b0f4b72
Adeola Ajayi <lilade2019@outlook.com>
2023-03-07 09:33:30 -0500

    new

---
+++ IAC for APP/variables.tf
@@ -0,0 +1,26 @@
+variable "app_plan_name" {
+  type        = "Adeola-BH-App-Plan"
+  description = "Azure App Service Plan Name"
+}
+
+variable "web_app_name" {
+  type        = "Adeola-BH-App"
+  description = "Azure Web App Name"
+}
+variable "registry_name" {
+  type        = "BHRegistry"
+  description = "Azure Web App Name"
+}
+variable "tag_name" {
+  type        = string
+  description = "Azure Web App Name"
+ default: 'latest'
+}
+variable "resource_group_name" {
+  type        = "BH-App-Group"
+  description = "Azure Resource Group Name"
+}
+variable "location" {
+  type        = "westeurope"
+  description = "Azure Resource Region Location"
+}
