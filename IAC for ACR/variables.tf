8bf90652f04903a3256e22ec895bd187dd498914
Adeola Ajayi <lilade2019@outlook.com>
2023-03-07 05:41:34 -0500

    newcommit

---
+++ IAC for ACR/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "Azure region to create infrastructure in"
+  default     = "westeurope"
+}
+variable "project" {
+  description = "The project name"
+  default     = "AdeolaBH"
+}
+variable "environment" {
+  description = "The project environment"
+  default     = "PROD"
+}
+variable "tags_extra" {
+  type        = map(string)
+  description = "Extra tags that should be applied to all resources"
+  default     = {}
+}
