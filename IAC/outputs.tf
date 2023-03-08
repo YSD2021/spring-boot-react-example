028a72a1c28edb379a52480bf9497afe5cadc145
Adeola Ajayi <lilade2019@outlook.com>
2023-03-08 05:46:18 -0500

    adjusted variable file

--- IAC/outputs.tf
+++ IAC/outputs.tf
@@ -1,3 +1,9 @@
-output "registry_id" {
-  value = azurerm_container_registry.registry.id
+output "login_server" {
+  value = data.azurerm_container_registry.registry.login_server
+}
+output "admin_username" {
+  value = data.azurerm_container_registry.registry.admin_username
+}
+output "admin_password" {
+  value = data.azurerm_container_registry.registry.admin_password
 }
