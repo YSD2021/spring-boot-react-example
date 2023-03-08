969c4c457f5a358573063221dcf400593d5efc82
Adeola Ajayi <lilade2019@outlook.com>
2023-03-08 05:11:03 -0500

    adjusted variable file

--- IAC/webapp.tf
+++ IAC/webapp.tf
@@ -1,41 +1,30 @@
 resource "azurerm_service_plan" "my_service_plan" {
- name                = "Ade-BH-AppPlan"
- location            = "westeurope"
- resource_group_name = "AdeolaBHresource-group"
- kind                = "Linux"
- reserved            = true
-
- sku {
-    tier = "Standard"
-    size = "S1"
- }
+  name                = "Ade-BH-AppPlan"
+  resource_group_name = "AdeolaBHresource-group"
+  location            = "westeurope"
+  os_type             = "Linux"
+  sku_name            = "s1"
 }
 
-locals {
- env_variables = {
-   DOCKER_REGISTRY_SERVER_URL            = "adeolabhregistry.azurecr.io"
-   DOCKER_REGISTRY_SERVER_USERNAME       = var.SERVER_USERNAME
-   DOCKER_REGISTRY_SERVER_PASSWORD       = var.SERVER_PASSWORD
-       WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
-	WEBSITES_PORT = 8080
- }
+  data "azurerm_container_registry" "registry" {
+  name                = "adeolabhregistry"
+  resource_group_name = "AdeolaBHresource-group"
 }
-resource "azurerm_app_service" "my_app_service_container" {
+resource "azurerm_app_service" "backend" {
  name                = "Ade-BH-App"
  location            = azurerm_service_plan.my_service_plan.location
  resource_group_name     = azurerm_service_plan.my_service_plan.resource_group_name
  app_service_plan_id     = azurerm_service_plan.my_service_plan.id
- site_config {
-   always_on = "true"
-
-   linux_fx_version  = "DOCKER|adeolabhregistry.azurecr.io/adeimage-ysd2021/spring-boot-react-example:82" #define the images to usecfor you application
- }
-
- app_settings = local.env_variables 
-}
-
-resource "azurerm_application_insights" "my_app_insight" {
- name                = "my_app_insight"
- location            = azurerm_service_plan.my_service_plan.location
- resource_group_name = azurerm_service_plan.my_service_plan.resource_group_name
- }
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL          = azurerm_container_registry.registry.login_server
+    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.registry.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.registry.admin_password
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+    WEBSITES_PORT                       = local.environmentvars["backend_port"]
+  }
+  site_config {
+    always_on = "true"
+    # define the images to used for you application
+    linux_fx_version = "DOCKER|adeolabhregistry.azurecr.io/adeimage-ysd2021/spring-boot-react-example:82"
+  }
+  }
