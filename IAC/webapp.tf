65ab219677371a7068c76d66ad726f046e11b276
Adeola Ajayi <lilade2019@outlook.com>
2023-03-08 07:10:27 -0500

    adjusted variable file

--- IAC/webapp.tf
+++ IAC/webapp.tf
@@ -1,5 +1,5 @@
 resource "azurerm_service_plan" "my_service_plan" {
-  name                = "Ade-BH-WebAppPlans"
+  name                = "Ade-BH-WebAppPlan"
   resource_group_name = "AdeolaBHresource-group"
   location            = "westeurope"
   os_type             = "Linux"
@@ -17,7 +17,7 @@ resource "azurerm_service_plan" "my_service_plan" {
   application_type    = "web"
 }
 resource "azurerm_app_service" "backend" {
- name                = "Ade-BH-WebApps"
+ name                = "Ade-BH-WebApp"
  location            = azurerm_service_plan.my_service_plan.location
  resource_group_name     = azurerm_service_plan.my_service_plan.resource_group_name
  app_service_plan_id     = azurerm_service_plan.my_service_plan.id
