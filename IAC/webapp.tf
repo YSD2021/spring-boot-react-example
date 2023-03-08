0460065fad095406e797f14ddcce9333d59acdd7
Adeola Ajayi <lilade2019@outlook.com>
2023-03-08 05:54:33 -0500

    adjusted variable file

--- IAC/webapp.tf
+++ IAC/webapp.tf
@@ -9,15 +9,6 @@ resource "azurerm_service_plan" "my_service_plan" {
   data "azurerm_container_registry" "registry" {
   name                = "adeolabhregistry"
   resource_group_name = "AdeolaBHresource-group"
-  output "login_server" {
-  value = data.azurerm_container_registry.registry.login_server
-}
-output "admin_username" {
-  value = data.azurerm_container_registry.registry.admin_username
-}
-output "admin_password" {
-  value = data.azurerm_container_registry.registry.admin_password
-}
 }
 resource "azurerm_app_service" "backend" {
  name                = "Ade-BH-Apps"
@@ -25,9 +16,9 @@ resource "azurerm_app_service" "backend" {
  resource_group_name     = azurerm_service_plan.my_service_plan.resource_group_name
  app_service_plan_id     = azurerm_service_plan.my_service_plan.id
   app_settings = {
-    DOCKER_REGISTRY_SERVER_URL          = azurerm_container_registry.registry.login_server
-    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.registry.admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD     = azurerm_container_registry.registry.admin_password
+    DOCKER_REGISTRY_SERVER_URL          = data.azurerm_container_registry.registry.login_server
+    DOCKER_REGISTRY_SERVER_USERNAME     = data.azurerm_container_registry.registry.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD     = data.azurerm_container_registry.registry.admin_password
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
     WEBSITES_PORT                       = 8080
   }
