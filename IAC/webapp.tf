fdbc11c1771a81530b6691385dd17e835ba5dd33
Adeola Ajayi <lilade2019@outlook.com>
2023-03-08 07:06:59 -0500

    adjusted variable file

--- IAC/webapp.tf
+++ IAC/webapp.tf
@@ -1,23 +1,25 @@
-data "azurerm_resource_group" "group" {
-  name = "AdeolaBHresource-group"
-}
-
 resource "azurerm_service_plan" "my_service_plan" {
   name                = "Ade-BH-WebAppPlans"
-  resource_group_name = data.azurerm_resource_group.group.name
-  location            = data.azurerm_resource_group.group.name.location
+  resource_group_name = "AdeolaBHresource-group"
+  location            = "westeurope"
   os_type             = "Linux"
   sku_name            = "S1"
 }
 
   data "azurerm_container_registry" "registry" {
   name                = "adeolabhregistry"
-  resource_group_name = data.azurerm_resource_group.group.name
+  resource_group_name = "AdeolaBHresource-group"
+}
+  resource "azurerm_application_insights" "insights" {
+  name                = "BH-appinsights"
+  location            = azurerm_service_plan.my_service_plan.location
+  resource_group_name = azurerm_service_plan.my_service_plan.resource_group_name
+  application_type    = "web"
 }
 resource "azurerm_app_service" "backend" {
  name                = "Ade-BH-WebApps"
- location            = data.azurerm_resource_group.group.name.location
- resource_group_name     = data.azurerm_resource_group.group.name
+ location            = azurerm_service_plan.my_service_plan.location
+ resource_group_name     = azurerm_service_plan.my_service_plan.resource_group_name
  app_service_plan_id     = azurerm_service_plan.my_service_plan.id
   app_settings = {
     DOCKER_REGISTRY_SERVER_URL          = data.azurerm_container_registry.registry.login_server
@@ -25,6 +27,7 @@ resource "azurerm_app_service" "backend" {
     DOCKER_REGISTRY_SERVER_PASSWORD     = data.azurerm_container_registry.registry.admin_password
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
     WEBSITES_PORT                       = 8080
+	 "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.insights.instrumentation_key}"
   }
   site_config {
     always_on = "true"
@@ -32,9 +35,3 @@ resource "azurerm_app_service" "backend" {
     linux_fx_version = "DOCKER|adeolabhregistry.azurecr.io/adeimage-ysd2021/spring-boot-react-example:82"
   }
   }
-  resource "azurerm_application_insights" "insights" {
-  name                = "BH-appinsights"
-  location            = data.azurerm_resource_group.group.name.location
-  resource_group_name = data.azurerm_resource_group.group.name
-  application_type    = "web"
-}
